478b90336d447b94ca7bf4412a0263d596c9c335
Violetstella <103453806+Violetstella@users.noreply.github.com>
2026-05-29 15:59:41 -0300

    Movimento de andar

---
+++ Assets/PersonagemController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;  
+    public float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+            rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        if(rb2d.velocity.magnitude < 5)
+        {
+            rb2d.velocity += new Vector2(vel,0)* HorizontalInput *Time.deltaTime;
+        }
+    }
+}
